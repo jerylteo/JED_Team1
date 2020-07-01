08b6c67152e0d072f93072a2f8aa520969d47dd3
Jeryl-HOME <jerylteo1337@gmail.com>
2020-07-01 13:26:19 +0800

    Empty cart servlet URL: /cart

---
+++ src/java/sg/edu/nyp/CartServlet.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nyp;
+
+import java.io.IOException;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author klmch
+ */
+@WebServlet(name = "CartServlet", urlPatterns = {"/cart"})
+public class CartServlet extends HttpServlet {
+
+
+    @Resource(name="jdbc/jed_lab7")
+    private DataSource dsCart;
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "doGet - retrieve cart. doPost - confirmation";
+    }// </editor-fold>
+
+}
