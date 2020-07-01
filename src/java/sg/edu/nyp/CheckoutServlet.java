b85b385c538a96b7fc5289ea39803aed25595cbd
LaxRav <LaxRav@LAPTOP-SO1CPLQ3>
2020-07-01 13:37:07 +0800

    Checkout Servlet

--- src/java/sg/edu/nyp/CheckoutServlet.java
+++ src/java/sg/edu/nyp/CheckoutServlet.java
@@ -70,7 +70,7 @@ public class CheckoutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
     }
 
     /**
